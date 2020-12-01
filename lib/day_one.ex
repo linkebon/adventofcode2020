7157b70d3b70206156fe07cc0a0bd0b4ecfcc70e
Staffan Holmgren <staffan.holmgren@tre.se>
2020-12-01 09:23:23 +0100

    DayOne b

--- lib/day_one.ex
+++ lib/day_one.ex
@@ -7,6 +7,13 @@ defmodule DayOne do
     for x <- list, y <- list, x + y == 2020, do: IO.puts("x*y=#{x*y}")
   end
 
+  def b() do
+    list = Read_File_Utils.read_file("one.txt")
+           |> Read_File_Utils.map_list_to_int()
+
+    for x <- list, y <- list, z <- list, x + y + z == 2020, do: IO.puts("x*y*z=#{x*y*z}")
+  end
+
 end
 
 
